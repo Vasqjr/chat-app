b53f28dc012b18ba2896fb92fcfb5736c48a0ad8
Vasqjr <vasqjr@farmingdale.educlear>
2024-05-06 10:16:32 -0400

    Placed Login page in ChakraProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import Login from "../components/Login"
+import { ChakraProvider } from "@chakra-ui/react"
 
 export default function App({ Component, pageProps }) {
   // return <Component {...pageProps} />;
-
-  return <Login />
+  return (
+    <ChakraProvider>
+      <Login />
+    </ChakraProvider>
+  )
 }
