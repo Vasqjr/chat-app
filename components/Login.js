d1b44f39a24bea3162a02ed4f4f8ccae0a78bd88
Vasqjr <vasqjr@farmingdale.educlear>
2024-05-06 10:16:05 -0400

    Updated Login Screen To Display Chat Icon Box
    - Imported Head from next/head
    - Imported ChatIcon from Chakra-ui Icons
    - Imported Box from Chakra-ui React library
    - Placed ChatIcon object within Box, formatted Box and ChatIcon for a cleaner look

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,24 @@
+import Head from "next/head";
+import { ChatIcon } from '@chakra-ui/icons';
+import { Box } from "@chakra-ui/react";
+
 export default function Login() {
     return (
-        <Head>
-        
-        </Head>
-    )
+        <>
+            <Head>
+                <title>Login</title>
+            </Head>
+
+            <Box
+                bgColor="rgb(127,0,255)"
+                w="fit-content"
+                padding={5}
+                rounded="3xl"
+            >
+                <ChatIcon w="100px" h="100px" color="rgb(255,255,255)" />
+            </Box>
+            
+    
+        </>
+        )
 }
